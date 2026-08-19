12ea4876f89755c3ff2d834d59f740674a0883d8
Nofy261 <noufy33@yahoo.com>
2026-08-19 17:25:07 +0200

    modif p2 server.sh

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -5,7 +5,7 @@ IFACE=$(ip -o addr show | awk '$4 ~ /^192.168.56./ {print $2}' | head -1)
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --flannel-iface=$IFACE --tls-san=192.168.56.110 --write-kubeconfig-mode 644" sh -s -
 
 while [ ! -f /var/lib/rancher/k3s/server/node-token ]; do
-sleep 2
+    sleep 2
 done
 
 cp /var/lib/rancher/k3s/server/node-token /vagrant/token
