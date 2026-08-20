7882f1148ec104446d0cb10dd3e262e200db65a7
ppuivif <papuivif@gmail.com>
2026-08-20 11:04:02 +0200

    modified:   bonus/scripts/start.sh 	modified:   values-minikube-minimum.yaml

--- bonus/scripts/start.sh
+++ bonus/scripts/start.sh
@@ -21,7 +21,8 @@ helm repo update
 
 helm upgrade --install gitlab gitlab/gitlab \
   --namespace "$GITLAB_NAMESPACE" \
-  --values https://gitlab.com/gitlab-org/charts/gitlab/raw/master/examples/values-minikube-minimum.yaml \
+#  --values https://gitlab.com/gitlab-org/charts/gitlab/raw/master/examples/values-minikube-minimum.yaml \
+  --values ./values-minikube-minimum.yaml \
   --set global.hosts.domain=k3d.gitlab.com \
   --set global.hosts.externalIP=0.0.0.0 \
   --set global.hosts.https=false \
