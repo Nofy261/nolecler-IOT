8409784d2c23ebff34de22de83850c09bdce40ba
ppuivif <papuivif@gmail.com>
2026-09-03 17:43:27 +0200

    modified:   bonus/scripts/install.sh

--- bonus/scripts/install.sh
+++ bonus/scripts/install.sh
@@ -2,7 +2,12 @@
 set -euo pipefail
 
 HELM_INSTALLER="https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4"
+GITLAB_SCRIPT_FOR_DEPENDENCIES="https://gitlab.com/gitlab-org/charts/gitlab.git"
 
 echo -e "\n\e[32;1m[Helm Installation]\e[0m\n"
 sleep 1
 curl -fsSL "$HELM_INSTALLER" | bash
+
+git clone "$GITLAB_SCRIPT_FOR_DEPENDENCIES"
+bash ./scripts/dev_dependencies.sh setup
+
