64dba7e50770d57174e6998b98ccb1a3005371cb
ppuivif <papuivif@gmail.com>
2026-09-04 12:24:00 +0200

    modified:   p1/scripts/install.sh

--- p1/scripts/install.sh
+++ p1/scripts/install.sh
@@ -6,9 +6,18 @@ fi
 
 apt update
 
-wget -O - https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
+if [ ! -f /usr/share/keyrings/hashicorp-archive-keyring.gpg ]; then
+    wget -O - https://apt.releases.hashicorp.com/gpg | \
+        gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+fi
+
+if [ ! -f /etc/apt/sources.list.d/hashicorp.list ]; then
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" \
+        | tee /etc/apt/sources.list.d/hashicorp.list
+fi
+
 apt update
+
 apt install -y vagrant
 
 apt install -y virtualbox
