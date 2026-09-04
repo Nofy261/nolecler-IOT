f785a8e4659b8e5d850fa659f164356da1f6be59
ppuivif <papuivif@gmail.com>
2026-09-04 15:52:28 +0200

    modified:   p1/scripts/clean.sh

--- p1/scripts/clean.sh
+++ p1/scripts/clean.sh
@@ -2,15 +2,15 @@
 
 set -euo pipefail
 
-source config.sh
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+PROJECT_DIR="$(dirname "$SCRIPT_DIR")"
+
+source "${SCRIPT_DIR}/config.sh"
 
 if [ "$EUID" -eq 0 ]; then
     echo -e "${RED}Error : do not execute this script with sudo.${NC}"
     exit 1
 fi
-
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-PROJECT_DIR="$(dirname "$SCRIPT_DIR")"
 
 cd "$PROJECT_DIR" || exit 1
 
