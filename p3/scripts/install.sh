dd656c4d64cb24944887fa793a0a3217e2a31100
ppuivif <papuivif@gmail.com>
2026-09-16 14:51:09 +0200

    new file:   p3/scripts/config.sh (for colors) 	modified:   p3/scripts/install.sh (colors added in messages to display)

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -2,8 +2,13 @@
 
 set -euo pipefail
 
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+PROJECT_DIR="$(dirname "$SCRIPT_DIR")"
+
+source "${SCRIPT_DIR}/config.sh"
+
 # Tools install
-echo -e "/nInstalling tools .../n"
+echo -e "${GREEN}\nInstalling tools ...\n${NC}"
 sudo apt-get update
 sudo apt-get install ca-certificates curl netcat-openbsd -y
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -12,7 +17,7 @@ sudo install -m 0755 -d /etc/apt/keyrings
 ARCH=$(dpkg --print-architecture)
 
 # Download GPG Docker key
-echo -e "/nDownloading Docker GPG key .../n"
+echo -e "${GREEN}\nDownloading Docker GPG key ...\n${NC}"
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
